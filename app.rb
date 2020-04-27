bdbe60b4c46def7b609818df7fca7d5ebb2f4299
mbrad26 <mbrad26@gmail.com>
2020-04-27 17:53:05 +0100

    created the views directory and moved the html in index.erb

--- app.rb
+++ app.rb
@@ -9,15 +9,6 @@ get '/secret' do
   'Hi there!'
 end
 
-get '/newsecret' do
-  'Hello!!!'
-end
-
 get '/cat' do
-  "<style>
-    .img-container { text-align: center }
-   </style>
-   <div class='img-container'>
-     <img src='http://bit.ly/1eze8aE' style='border: 3px dashed red'>
-   </div>"
+  erb(:index)
 end
